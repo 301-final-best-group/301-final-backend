181936b5958d5e5977a81489cac5ac97aa39f237
yurii hlukhyi <yhlukhyi@gmail.com>
2023-03-09 13:19:30 -0800

    try catch

--- modules/getPlacesApi.js
+++ modules/getPlacesApi.js
@@ -11,82 +11,91 @@ class Place {
 }
 
 async function getPlacesForCity(searchQuery, apiKey) {
-    if (!searchQuery) {
-        throw new Error('bad request');
-    }
-
-    let urlApi = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${searchQuery}&format=json`
-    let responseApi = await axios.get(urlApi);
-    let lat = (responseApi.data[0].lat)
-    let lon = (responseApi.data[0].lon)
-
-    let attractionPlacesUrl = `https://api.content.tripadvisor.com/api/v1/location/nearby_search?latLong=${lat}%2C${lon}&key=${apiKey}&category=attractions&language=en`
-
-    let placesResponse = await axios.get(attractionPlacesUrl);
-    if (placesResponse) {
-        let res = []
-        for (let index = 0; index < placesResponse.data.data.length; index++) {
-            const constUpdatedPlace = placesResponse.data.data[index];
-            const placeId = constUpdatedPlace.location_id
-            let locationDetailsUrl = `https://api.content.tripadvisor.com/api/v1/location/${placeId}/details?key=${apiKey}&language=en&currency=USD`
-            const placeDetails = await axios.get(locationDetailsUrl);
-            if (placeDetails) {
-                constUpdatedPlace["description"] = placeDetails.data.description
-                constUpdatedPlace["address"] = placeDetails.data.address_obj.address_string
-            }
-
-            let imagesUrl = `https://api.content.tripadvisor.com/api/v1/location/${placeId}/photos?key=${apiKey}&language=en`
-            let imagesResponse = await axios.get(imagesUrl);
-            if (imagesResponse) {
-                constUpdatedPlace["images"] = imagesResponse.data.data.map(obj => obj.images.large.url)
-            }
-            res.push(constUpdatedPlace)
+    try {
+        if (!searchQuery) {
+            throw new Error('bad request');
         }
-        let finalRes = res.map(place => new Place(place))
-        return finalRes
-    } else {
-        throw new Error('places not found');
+
+        let urlApi = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${searchQuery}&format=json`
+        let responseApi = await axios.get(urlApi);
+        let lat = (responseApi.data[0].lat)
+        let lon = (responseApi.data[0].lon)
+
+        let attractionPlacesUrl = `https://api.content.tripadvisor.com/api/v1/location/nearby_search?latLong=${lat}%2C${lon}&key=${apiKey}&category=attractions&language=en`
+
+        let placesResponse = await axios.get(attractionPlacesUrl);
+        if (placesResponse) {
+            let res = []
+            for (let index = 0; index < placesResponse.data.data.length; index++) {
+                const constUpdatedPlace = placesResponse.data.data[index];
+                const placeId = constUpdatedPlace.location_id
+                let locationDetailsUrl = `https://api.content.tripadvisor.com/api/v1/location/${placeId}/details?key=${apiKey}&language=en&currency=USD`
+                const placeDetails = await axios.get(locationDetailsUrl);
+                if (placeDetails) {
+                    constUpdatedPlace["description"] = placeDetails.data.description
+                    constUpdatedPlace["address"] = placeDetails.data.address_obj.address_string
+                }
+
+                let imagesUrl = `https://api.content.tripadvisor.com/api/v1/location/${placeId}/photos?key=${apiKey}&language=en`
+                let imagesResponse = await axios.get(imagesUrl);
+                if (imagesResponse) {
+                    constUpdatedPlace["images"] = imagesResponse.data.data.map(obj => obj.images.large.url)
+                }
+                res.push(constUpdatedPlace)
+            }
+            let finalRes = res.map(place => new Place(place))
+            return finalRes
+        } else {
+            throw new Error('places not found');
+        }
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
 }
 
 async function getRestaurantForCity(searchQuery, apiKey) {
-    if (!searchQuery) {
-        throw new Error('bad request');
-    }
-
-    let urlApi = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${searchQuery}&format=json`
-    let responseApi = await axios.get(urlApi);
-    let lat = (responseApi.data[0].lat)
-    let lon = (responseApi.data[0].lon)
-
-    let attractionPlacesUrl = `https://api.content.tripadvisor.com/api/v1/location/nearby_search?latLong=${lat}%2C${lon}&key=${apiKey}&category=restaurants&language=en`
-
-    let placesResponse = await axios.get(attractionPlacesUrl);
-    if (placesResponse) {
-        let res = []
-        for (let index = 0; index < placesResponse.data.data.length; index++) {
-            const constUpdatedPlace = placesResponse.data.data[index];
-            const placeId = constUpdatedPlace.location_id
-            let locationDetailsUrl = `https://api.content.tripadvisor.com/api/v1/location/${placeId}/details?key=${apiKey}&language=en&currency=USD`
-            const placeDetails = await axios.get(locationDetailsUrl);
-            if (placeDetails) {
-                constUpdatedPlace["description"] = placeDetails.data.description
-                constUpdatedPlace["address"] = placeDetails.data.address_obj.address_string
-            }
-
-            let imagesUrl = `https://api.content.tripadvisor.com/api/v1/location/${placeId}/photos?key=${apiKey}&language=en`
-            let imagesResponse = await axios.get(imagesUrl);
-            if (imagesResponse) {
-                constUpdatedPlace["images"] = imagesResponse.data.data.map(obj => obj.images.large.url)
-            }
-
-            res.push(constUpdatedPlace)
+    try {
+        if (!searchQuery) {
+            throw new Error('bad request');
         }
-        let finalRes = res.map(place => new Place(place))
-        return finalRes
-    } else {
-        throw new Error('places not found');
+
+        let urlApi = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${searchQuery}&format=json`
+        let responseApi = await axios.get(urlApi);
+        let lat = (responseApi.data[0].lat)
+        let lon = (responseApi.data[0].lon)
+
+        let attractionPlacesUrl = `https://api.content.tripadvisor.com/api/v1/location/nearby_search?latLong=${lat}%2C${lon}&key=${apiKey}&category=restaurants&language=en`
+
+        let placesResponse = await axios.get(attractionPlacesUrl);
+        if (placesResponse) {
+            let res = []
+            for (let index = 0; index < placesResponse.data.data.length; index++) {
+                const constUpdatedPlace = placesResponse.data.data[index];
+                const placeId = constUpdatedPlace.location_id
+                let locationDetailsUrl = `https://api.content.tripadvisor.com/api/v1/location/${placeId}/details?key=${apiKey}&language=en&currency=USD`
+                const placeDetails = await axios.get(locationDetailsUrl);
+                if (placeDetails) {
+                    constUpdatedPlace["description"] = placeDetails.data.description
+                    constUpdatedPlace["address"] = placeDetails.data.address_obj.address_string
+                }
+
+                let imagesUrl = `https://api.content.tripadvisor.com/api/v1/location/${placeId}/photos?key=${apiKey}&language=en`
+                let imagesResponse = await axios.get(imagesUrl);
+                if (imagesResponse) {
+                    constUpdatedPlace["images"] = imagesResponse.data.data.map(obj => obj.images.large.url)
+                }
+
+                res.push(constUpdatedPlace)
+            }
+            let finalRes = res.map(place => new Place(place))
+            return finalRes
+        } else {
+            throw new Error('places not found');
+        }
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error getting restaurant places');
     }
 }
-
 module.exports = { getPlacesForCity, getRestaurantForCity };
